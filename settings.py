0baf7584361500e82d95d5f3740b02f1b5d77f36
hoksly <realhoksly@gmail.com>
2022-01-12 14:16:29 +0200

    V.1.0.2

--- settings.py
+++ settings.py
@@ -1,8 +1,4 @@
-TOKEN = "token"
-PATH = "data"
-CONTACT = "@username"
-GROUP_ID = 1 #your channel
-'''
+
 from environs import Env
 env = Env()
 env.read_env()
@@ -10,6 +6,3 @@ env.read_env()
 PATH = "data"
 GROUP_ID = env.int("GROUP_ID")
 TOKEN = env.str('TOKEN')
-
-
-'''
